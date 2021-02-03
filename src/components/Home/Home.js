40f7cda582e4bfbc5669399e52771dc96996fdae
Gabriel Pasquale <pasqualeunq@gmail.com>
2021-02-03 18:08:51 -0300

    routers added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Matchs from "../Matchs"
 
 import "./Home.scss";
@@ -9,16 +10,35 @@ export default function Home(props) {
     <div className="body2">
 
       <div className="sidebar">
-        <a className="active" href="#home">Partidos</a>
+        <a className="active" href="/">Partidos</a>
         {/* <a href="#home">Home</a> */}
-        <a href="#news">Equipos</a>
-        <a href="#contact">Jugadores</a>
-        <a className="abajo1" href="#about">Nuevo Partido</a>
-        <a className="abajo2" href="#about">Nuevo Equipo</a>
-        <a className="abajo3" href="#about">Nuevo Jugador</a>
+        <a href="teams">Equipos</a>
+        <a href="players">Jugadores</a>
+        <a className="abajo1" href="match">Nuevo Partido</a>
+        <a className="abajo2" href="team">Nuevo Equipo</a>
+        <a className="abajo3" href="player">Nuevo Jugador</a>
       </div>
-
-      <Matchs />
+   
+      <Switch>
+        <Route path="/" exact>
+          <Matchs /> 
+        </Route>
+        <Route path="/teams" exact>
+          <div className="borrar"> <h1>teams</h1> </div>
+        </Route>
+        <Route path="/players" exact>
+          <div className="borrar"> <h1>players</h1> </div>
+        </Route>
+        <Route path="/match" exact>
+          <div className="borrar"> <h1>match</h1> </div>
+        </Route>
+        <Route path="/team" exact>
+          <div className="borrar"> <h1>team</h1> </div>
+        </Route>
+        <Route path="/player" exact>
+          <div className="borrar"> <h1>player</h1> </div>
+        </Route>
+      </Switch>
 
     </div>
     
